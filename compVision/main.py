c98a9317d26be485c57864778d5dbd1d477f0654
Max Letellier <mletellier02@gmail.com>
2023-06-11 21:04:21 +0100

    smol changes

--- compVision/main.py
+++ compVision/main.py
@@ -1,10 +1,8 @@
-import round_score as rs
-import warp_img as wImg
+import warp_img as wImg, helper as hp, round_score as rs
 from detecto import visualize
-import helper as hp
 
 def main():
-    # wImg.unwarp_img('Game_Img/test') # Toggle
+    # table = wImg.unwarp_img('Game_Img/test') # Toggle
     labels, boxes, scores, image = hp.load_model('image') # Will need to find a way to get latest image
     center_darts, labels, boxes, scores = rs.clean_data(labels, boxes, scores)
     # print(boxes)
@@ -16,10 +14,8 @@ def main():
     # hp.plot_center(center_darts, image)
     rs.recalabrate(closest, dart_score)
 
-if __name__ == '__main__':
-    main()
-
 # Notes:
 # python labelImg.py -- Labeling Software
 # Noted that how I labelled the target as a whole not the top part. Causes issue which popdart is the closes. v2 of the labelled images has target labelled differently
 # Note that (atm with the camera that is fish eyed) the image fish eyed lens hasn't been removed. This affects on games that are really close in the corder of the table
+# Place this main file in app.py!!!!!!!!!!!
