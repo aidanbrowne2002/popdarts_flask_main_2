8a109daf7be0762c8c03d6133c4b107a0c3f6511
Max Letellier <mletellier02@gmail.com>
2023-06-13 13:52:29 +0100

    fix HTML

--- helperF.py
+++ helperF.py
@@ -40,4 +40,4 @@ def generate_frames():
             ret, buffer = cv2.imencode('.jpg',frame)
             frame = buffer.tobytes()
         yield(b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') # generates the next frame 
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') # generates the next frame
